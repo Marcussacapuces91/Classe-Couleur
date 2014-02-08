ce79a45d6fc2a31d5b9d1cc8988204667ac67778
Marc Sibert <marc@sibert.fr>
2014-02-09 00:52:30 +0100

    update luminance functions new colors

--- couleur.cpp
+++ couleur.cpp
@@ -24,26 +24,74 @@ byte Couleur::getLuminance() const {
 }
 
 Couleur& Couleur::setLuminance(const byte aLum) {
-  const unsigned l = getLuminance();
+  const byte l = getLuminance();
   if (l == 0) {
     fRed = 1;
     fGreen = 1;
     fBlue = 1;
   }
-  fRed = unsigned(fRed) * aLum / l;
-  fGreen = unsigned(fGreen) * aLum / l;
-  fBlue = unsigned(fBlue) * aLum / l;
+  fRed = min(unsigned(fRed) * aLum / l, 255);
+  fGreen = min(unsigned(fGreen) * aLum / l, 255);
+  fBlue = min(unsigned(fBlue) * aLum / l, 255);
+  return *this;    
+}
+
+Couleur& Couleur::setFiltre(const byte aRed, const byte aGreen, const byte aBlue) {
+  fRed = min(unsigned(fRed) * aRed / 255, 255);
+  fGreen = min(unsigned(fGreen) * aGreen / 255, 255);
+  fBlue = min(unsigned(fBlue) * aBlue / 255, 255);
   return *this;    
 }
 
 const Couleur Couleur::BLACK(0x000000);
-const Couleur Couleur::MAROON(0x800000);
 const Couleur Couleur::RED(0xFF0000);
-const Couleur Couleur::ORANGE(0xFFA500);
 const Couleur Couleur::YELLOW(0xFFFF00);
+const Couleur Couleur::LIME(0x00FF00);
 const Couleur Couleur::GREEN(0x008000);
 const Couleur Couleur::BLUE(0x0000FF);
 const Couleur Couleur::VIOLET(0xEE82EE);
 const Couleur Couleur::WHITE(0xFFFFFF);
 const Couleur Couleur::GRAY(0x808080);
+
+// Pink color
+const Couleur Couleur::PINK(0xFFC0CB);
+const Couleur Couleur::LIGHT_PINK(0xFFB6C1);
+const Couleur Couleur::HOT_PINK(0xFF69B4);
+const Couleur Couleur::DEEP_PINK(0xFF1493);
+const Couleur Couleur::MEDIUM_VIOLET_RED(0xC71585);
+const Couleur Couleur::PALE_VIOLET_RED(0xDB7093);
+
+// Orange color
+const Couleur Couleur::LIGHT_SALMON(0xFFA07A);
+const Couleur Couleur::CORAL(0xFF7F50);
+const Couleur Couleur::TOMATO(0xFF6347);
+const Couleur Couleur::ORANGE_RED(0xFF4500);
+const Couleur Couleur::DARK_ORANGE(0xFF8C00);
+const Couleur Couleur::ORANGE(0xFFA500);
+
+// Purple color
+const Couleur Couleur::DARK_MAGENTA(0x8B008B);
+const Couleur Couleur::PURPLE(0x800080);
+
+// Blue color
+const Couleur Couleur::LIGHT_BLUE(0xADD8E6);
+
+// Brown color
+const Couleur Couleur::CORNSILK(0xFFF8DC);
+const Couleur Couleur::BLANCHED_ALMOND(0xFFEBCD);
+const Couleur Couleur::BISQUE(0xFFE4C4);
+const Couleur Couleur::NAVAJO_WHITE(0xFFDEAD);
+const Couleur Couleur::WHEAT(0xF5DEB3);
+const Couleur Couleur::BURLY_WOOD(0xDEB887);
+const Couleur Couleur::TAN(0xD2B48C);
+const Couleur Couleur::ROSY_BROWN(0xBC8F8F);
+const Couleur Couleur::SANDY_BROWN(0xF4A460);
+const Couleur Couleur::GOLDENROD(0xDAA520);
+const Couleur Couleur::DARK_GOLDENROD(0xB8860B);
+const Couleur Couleur::PERU(0xCD853F);
+const Couleur Couleur::CHOCOLATE(0xD2691E);
+const Couleur Couleur::SADDLE_BROWN(0x8B4513);
+const Couleur Couleur::SIENNA(0xA0522D);
+const Couleur Couleur::BROWN(0xA52A2A);
+const Couleur Couleur::MAROON(0x800000);
 
--- couleur.h
+++ couleur.h
@@ -1,7 +1,7 @@
 #ifndef __COULEUR_H__
 #define __COULEUR_H__
 
-#include "Arduino.h"   // Arduino defines
+#include <Arduino.h>   // Arduino defines
 
 class Couleur {
 
@@ -12,15 +12,56 @@ private:
   
 public:
   static const Couleur BLACK;
-  static const Couleur MAROON;
   static const Couleur RED;
-  static const Couleur ORANGE;
   static const Couleur YELLOW;
   static const Couleur GREEN;
+  static const Couleur LIME;
   static const Couleur BLUE;
   static const Couleur VIOLET;
   static const Couleur WHITE;
   static const Couleur GRAY;
+  
+// Pink color
+  static const Couleur PINK;
+  static const Couleur LIGHT_PINK;
+  static const Couleur HOT_PINK;
+  static const Couleur DEEP_PINK;
+  static const Couleur MEDIUM_VIOLET_RED;
+  static const Couleur PALE_VIOLET_RED;
+
+// Orange color
+  static const Couleur LIGHT_SALMON;
+  static const Couleur CORAL;
+  static const Couleur TOMATO;
+  static const Couleur ORANGE_RED;
+  static const Couleur DARK_ORANGE;
+  static const Couleur ORANGE;
+  
+// Purple color
+  static const Couleur DARK_MAGENTA;
+  static const Couleur PURPLE;
+  
+// Blue color
+  static const Couleur LIGHT_BLUE;
+
+// Brown color
+  static const Couleur CORNSILK;
+  static const Couleur BLANCHED_ALMOND;
+  static const Couleur BISQUE;
+  static const Couleur NAVAJO_WHITE;
+  static const Couleur WHEAT;
+  static const Couleur BURLY_WOOD;
+  static const Couleur TAN;
+  static const Couleur ROSY_BROWN;
+  static const Couleur SANDY_BROWN;
+  static const Couleur GOLDENROD;
+  static const Couleur DARK_GOLDENROD;
+  static const Couleur PERU;
+  static const Couleur CHOCOLATE;
+  static const Couleur SADDLE_BROWN;
+  static const Couleur SIENNA;
+  static const Couleur BROWN;
+  static const Couleur MAROON;
 
   Couleur();
   Couleur(const byte aRed, const byte aGreen, const byte aBlue);
@@ -28,7 +69,9 @@ public:
   
   byte getLuminance() const;
   Couleur& setLuminance(const byte aLum);
-  
+//   Couleur filtreLuminance(const byte aLum) const;
+  Couleur& setFiltre(const byte aRed, const byte aGreen, const byte aBlue);
+
   template <int R, int G, int B>
   static void afficher(const Couleur& aCouleur);
   
